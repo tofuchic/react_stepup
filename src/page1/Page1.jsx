a06069dcb657e0627f98ea9b488790ebb1d36ffc
tofuchic <blue_savant_syndrome@yahoo.co.jp>
2021-10-17 00:06:47 +0900

    Linkを使わないページ遷移を実装

--- src/page1/Page1.jsx
+++ src/page1/Page1.jsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = (count) => {
   count = 100;
   const arr = [...Array(count).keys()];
+  const history = useHistory();
+
+  const onClickDetailB = () => {
+    history.push("/page1/detailB");
+  };
+
   return (
     <div>
       <h1>Page1です</h1>
       <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailB}>Linkを使わないページ遷移</button>
     </div>
   );
 };
