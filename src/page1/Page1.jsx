dceeb9440c43a5eda3423989ec35c1b1ac6f1f53
tofuchic <blue_savant_syndrome@yahoo.co.jp>
2021-10-17 00:02:49 +0900

    useLocationでstateを渡したページ遷移を実装

--- src/page1/Page1.jsx
+++ src/page1/Page1.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
-export const Page1 = () => {
+export const Page1 = (count) => {
+  count = 100;
+  const arr = [...Array(count).keys()];
   return (
     <div>
       <h1>Page1です</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
     </div>
--- src/page1/Page1DetailA.jsx
+++ src/page1/Page1DetailA.jsx
@@ -1,7 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
   return (
     <div>
       <h2>Page1DetailAです</h2>
+      {state}
     </div>
   );
 };
