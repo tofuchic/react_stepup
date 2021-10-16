251e8c65b58a173f70ba78104584000cd05655fc
tofuchic <blue_savant_syndrome@yahoo.co.jp>
2021-10-17 00:10:23 +0900

    戻るボタンを実装

--- src/page1/Page1DetailB.jsx
+++ src/page1/Page1DetailB.jsx
@@ -1,7 +1,15 @@
+import { useHistory } from "react-router-dom";
+
 export const Page1DetailB = () => {
+  const history = useHistory();
+
+  const onClickBack = () => {
+    history.goBack();
+  };
   return (
     <div>
       <h2>Page1DetailBです</h2>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
