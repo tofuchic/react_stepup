1778f6b0dcddb14c28c266233438832ffb418297
tofuchic <blue_savant_syndrome@yahoo.co.jp>
2021-10-16 21:56:32 +0900

    不要なimportを削除

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,6 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
 import { Page2 } from "../Page2";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
 import { Page1Routes } from "./Page1Routes";
 
 export const Router = () => {
