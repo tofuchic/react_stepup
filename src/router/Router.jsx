b4d8124bbd21b449b890cd41cf29cc57a2ef803f
tofuchic <blue_savant_syndrome@yahoo.co.jp>
2021-10-16 21:55:54 +0900

    Page1配下のページをmapで表現

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import { Page1 } from "../Page1";
 import { Page2 } from "../Page2";
 import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetailB";
+import { Page1Routes } from "./Page1Routes";
 
 export const Router = () => {
   return (
@@ -16,15 +17,15 @@ export const Router = () => {
         render={({ match: { url } }) => (
           <Switch>
             {console.log(url)}
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
+            {Page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
           </Switch>
         )}
       />
